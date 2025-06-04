6216f5e479841ddfe3e73f4d1fd1bd20cda2d617
TanmayCJ <tannycjain@gmail.com>
2025-06-04 12:04:00 +0530

    backend stuff

---
+++ server.js
@@ -0,0 +1,34 @@
+
+const name = "Anakin Skywalker"
+const age = 45;
+const occupation = "Jedi Knight";
+const isAlive = false; 
+const planet = "Tatooine";
+const species = "Human";
+const properties = {" lightsaberColor": "blue", "forceSensitive": true, "master": "Obi-Wan Kenobi"};
+
+
+const character = {
+  name: name ,
+  age: age,
+  occupation: occupation,
+  isAlive: isAlive,
+  planet: planet,
+  species: species,
+  properties: properties
+}; 
+
+const description = {
+backstory: "Anakin Skywalker was a Jedi Knight who was prophesied to bring balance to the Force. However, he fell to the dark side and became Darth Vader.",
+abilities: ["Force Push", "Lightsaber Combat", "Jedi Mind Tricks"],
+allies: ["Obi-Wan Kenobi", "Padmé Amidala", "Ahsoka Tano"],
+enemies: ["Darth Sidious", "Count Dooku", "Boba Fett"],
+lore: "Anakain Sky walker was born on the planet Tatooine and was saved from poverty by his master Qui-Gon Jinn who was the master of Obi-Wan Kenobi. Anakin was trained as a Jedi and became one of the most powerful Jedi in the galaxy. However, faced with the loss of his mother and the fear of losing his wife Padmé Amidala, he was seduced by the dark side and became Darth Vader, serving under Emperor Palpatine. His story is one of tragedy, redemption, and the enduring struggle between good and evil.",
+feats: ["Defeated Count Dooku", "Saved the galaxy from the Sith", "Redeemed by his son Luke Skywalker"],
+}
+
+
+
+
+console.log(character);
+console.log(description);
